4d3ec0e825541f506e591ebf3e6aa6e4f23ff18d
ycx <553566463@qq.com>
2019-04-13 22:33:36 +0800

    (测试的提交)

---
+++ app/src/main/java/com/example/ycx36/recruitment/view/fragment/test.java
@@ -0,0 +1,11 @@
+package com.example.ycx36.recruitment.view.fragment;
+
+public class test {
+    int a ;
+
+     public int  test(){
+         int b = 1;
+         a = b;
+         return b;
+     }
+}
